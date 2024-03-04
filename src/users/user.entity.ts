ae56e660edb26fe7e53c5f1ec68def0fd2d9aa90
gualter <gualterparada@gmail.com>
2024-03-04 11:31:15 +0000

    add initial user logic

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
