8a2764339802a16355cc90d557b9890bcc6135f8
gualter <gualterparada@gmail.com>
2024-03-05 15:46:46 +0000

    add e2e tests

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -3,7 +3,7 @@ import { CanActivate, ExecutionContext } from '@nestjs/common';
 export class AuthGuard implements CanActivate {
   canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
-    const { userId } = request.session;
+    const { userId } = request.session || {};
 
     return userId;
   }
